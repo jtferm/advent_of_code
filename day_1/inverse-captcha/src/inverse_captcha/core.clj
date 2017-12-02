87fce1bb0b580d96ee056a45fe87b98e78ea164a
jferm <jtferm@gmail.com>
2017-12-01 18:35:43 -0600

    Advent of Code day 1 - Less vector-dependent implementation

--- day_1/inverse-captcha/src/inverse_captcha/core.clj
+++ day_1/inverse-captcha/src/inverse_captcha/core.clj
@@ -11,18 +11,13 @@
 
 (defn sum-same
   [numbers]
-  (let [nexts   (conj (subvec numbers 1) (first numbers))
-        matches (map matching-values numbers nexts)]
-    (reduce + matches)))
+  (let [nexts (rest (cycle numbers))]
+    (reduce + (map matching-values numbers nexts))))
 
 (defn char-to-digit
   [ch]
   (java.lang.Character/digit ch 10))
 
-(defn foo
-  [input]
-  (vec (map char-to-digit (seq input))))
-
 (defn -main
   [& args]
-  (println (sum-same (foo (first args)))))
+  (println (sum-same (map char-to-digit (seq (first args))))))
